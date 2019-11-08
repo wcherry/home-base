43d851fc42f081378fea6f18745a627fe8552075
William Cherry <wcherry69@gmail.com>
2019-11-08 10:02:08 -0800

    Display Articles in a Popup dialog https://github.com/wcherry/home-base/issues/7 - Display Articles in a Popup dialog * Added @zeit/next-css and next.config.js to allow processing of CSS files * Added react-bootstrap, bootstrap, and react-iframe to handle display of content in a modal popup * Refactored index.js Home from a function to a component * Modifed rss_feed component to allow parent ot pass call back to handle links being clicked * Added a 'Visit Site' button that will open a new tab and populate it with the article. * The height is hardcoded to the 600px, we will want to revisit the sizing of the modal and it internal iframe
    The way in which events bubble up to the parent component may not be keeping with React style guidelines and may need to be revisted. Also the user of the bootstrap styling may not be the long term branding strategy.

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,29 @@ import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
 import RssFeed from '../components/rss_feed'
+import ArticleModal from '../components/article_modal.js'
 
-const Home = () => (
-  <div>
-    <RssFeed></RssFeed>
-  </div>
+import css from 'bootstrap/dist/css/bootstrap.css'
 
-)
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {url: null, show: false}
+  }
+  
+  handleClick = (url) => {
+    console.log(`Clicked: ${url}`)
+    this.setState({url: url, show: true});
+    this.refs.popup.showUrl(url);
+  }
+
+  render = () => (
+    <div>
+      <ArticleModal ref="popup" display={this.state.show} url={this.state.url}></ArticleModal>
+      <RssFeed linkHandler={this.handleClick}></RssFeed>
+    </div>
+
+  )
+}
 
 export default Home
